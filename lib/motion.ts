ac1dd64d1aafa50f01852a5f2e32d3975a4634fb
ceretoon <ceretoon@gmail.com>
2025-04-07 20:00:45 +0200

    Add motion component wrappers

---
+++ lib/motion.ts
@@ -0,0 +1,9 @@
+
+
+import { motion } from "framer-motion";
+
+// Compatible with modern Framer Motion and Next.js + TypeScript
+export const MotionH1 = motion("h1");
+export const MotionP = motion("p");
+export const MotionDiv = motion("div");
+
